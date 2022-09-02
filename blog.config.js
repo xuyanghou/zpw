36d217918b163ae035a8502a899fa7cd812c2382
xuyanghou <92911519+xuyanghou@users.noreply.github.com>
2022-09-02 15:20:54 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: 'tangly1024', // 作者
-  BIO: '一个普通的干饭人🍚', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  AUTHOR: 'houxuyang', // 作者
+  BIO: 'hahahahaha', // 作者简介
+  LINK: 'https://persistent-animantarx-706.notion.site/a1c348e6f6e74dd4a380a70cf44128ac', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
